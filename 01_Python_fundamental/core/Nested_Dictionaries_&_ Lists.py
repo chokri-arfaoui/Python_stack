0aee5b51a95f8eb1bda4bdfff1c7e9877b1dd681
arfaoui chokri <arfaouichokri601@gmail.com>
2023-07-11 18:11:18 +0100

    test2

--- 01_Python_fundamental/core/Nested_Dictionaries_&_ Lists.py
+++ 01_Python_fundamental/core/Nested_Dictionaries_&_ Lists.py
@@ -58,7 +58,7 @@ dojo = {
 'locations': ['San Jose', 'Seattle', 'Dallas', 'Chicago', 'Tulsa', 'DC', 'Burbank'],
 'instructors': ['Michael', 'Amy', 'Eduardo', 'Josh', 'Graham', 'Patrick', 'Minh', 'Devon']
 }
-
+# btyy
 # for i in range(7):
 #     print (dojo['locations'][i])
 
